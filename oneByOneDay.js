319bd1943aebf0ded0261927d4aa8a522d8ca4f8
萧宇亨 <xiaoyuheng@haier.com>
2020-12-17 15:37:30 +0800

    变更 逛一逛 接口

--- oneByOneDay.js
+++ oneByOneDay.js
@@ -5,12 +5,14 @@ const USERCODE1 = process.env.USERCODE1;
 const USERCODE2 = process.env.USERCODE2;
 
 const Url = `https://www.xyoct.com/xiaochengxu/XiaoChengXuApi.aspx?type=QianDao&memberid=`;// 签到
+const chouUrl = 'https://www.xyoct.com/choujiang0528/webserver/AjaxApi.aspx?Type=ChouJiang&memberID=';// 抽奖
 
-const teaUrl =`https://www.xyoct.com/xiaochengxu/XiaoChengXuApi.aspx?type=WanChengChaYuanRenWu&move=Free&mid=`;//领取茶叶
-const signTeaUrl =`https://www.xyoct.com/xiaochengxu/XiaoChengXuApi.aspx?type=WanChengChaYuanRenWu&move=Sign&mid=`; // 签到
+
+const teaUrl = `https://www.xyoct.com/xiaochengxu/XiaoChengXuApi.aspx?type=WanChengChaYuanRenWu&move=Free&mid=`;//领取茶叶
+const signTeaUrl = `https://www.xyoct.com/xiaochengxu/XiaoChengXuApi.aspx?type=WanChengChaYuanRenWu&move=Sign&mid=`; // 签到
 const shareTeaUrl = 'https://www.xyoct.com/xiaochengxu/XiaoChengXuApi.aspx?type=WanChengChaYuanRenWu&move=Share&mid='; //分享
 const applyTeaUrl = `https://www.xyoct.com/xiaochengxu/XiaoChengXuApi.aspx?type=ShiFei&mid=`; //施肥
-const guangTeaUrl = `https://www.xyoct.com/xiaochengxu/XiaoChengXuApi.aspx?type=HuoDongHuiGu&cls=AC001&page=1&mid=`// 逛一逛
+const guangTeaUrl = `https://www.xyoct.com/xiaochengxu/XiaoChengXuApi.aspx?type=WanChengChaYuanRenWu&move=View&mid=`// 逛一逛
 
 // const zanUR=`https://www.xyoct.com/xiaochengxu/XiaoChengXuApi.aspx?type=DianZan`; // 点赞
 // 45
@@ -20,34 +22,50 @@ const guangTeaUrl = `https://www.xyoct.com/xiaochengxu/XiaoChengXuApi.aspx?type=
 // }
 
 function getScore (mid) {
-  getMessage(Url+mid);
-  getMessage(teaUrl+mid);
-  getMessage(signTeaUrl+mid);
-  //分享三次
-  getMessage(shareTeaUrl+mid);
-  getMessage(shareTeaUrl+mid);
-  getMessage(shareTeaUrl+mid);
-  getMessage(Url+mid);
-  getMessage(guangTeaUrl+mid);
+  // 签到
+  getMessage(Url + mid);
+  // 抽奖 三次机会
+  postMessage(chouUrl + mid);
+  postMessage(chouUrl + mid);
+  postMessage(chouUrl + mid);
+  // 茶叶相关
+  getMessage(teaUrl + mid);
+  getMessage(signTeaUrl + mid);
+  getMessage(shareTeaUrl + mid);//分享三次
+  getMessage(shareTeaUrl + mid);
+  getMessage(shareTeaUrl + mid);
+  getMessage(Url + mid);
+  getMessage(guangTeaUrl + mid);
   // getMessage(applyTeaUrl+mid);
 }
 
 getScore(USERCODE1);
 getScore(USERCODE2);
 
-function getMessage(url){
+function getMessage (url) {
   axios({
-    url:url,
+    url: url,
     method: 'get',
     userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E217 MicroMessenger/6.8.0(0x16080000) NetType/WIFI Language/en Branch/Br_trunk MiniProgramEnv/Mac',
-  }).then(res=>{
+  }).then(res => {
     console.log(res.data)
-    if(res.data.State===-1){
+    if (res.data.State === -1) {
       log(res.data);
-    }else {
+    } else {
       log(res.data);
     }
   })
+}
+
+function postMessage (url) {
+  axios({
+    url: url,
+    method: 'post',
+    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E217 MicroMessenger/6.8.0(0x16080000) NetType/WIFI Language/en Branch/Br_trunk MiniProgramEnv/Mac',
+    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+  }).then(res => {
+    console.log('responseData', res.data)
+  }).catch(e => console.log('error', e))
 }
 
 function log (data) {
@@ -57,13 +75,13 @@ function log (data) {
 }
 
 
-function toTime() {
+function toTime () {
   const date = new Date();
   const Y = date.getFullYear() + '-';
   const M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
-  const D = (date.getDate() < 10 ? '0' + (date.getDate() ) : date.getDate()) + ' ';
+  const D = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate()) + ' ';
   const h = date.getHours() + ':';
-  const m = date.getMinutes()+'   ';
+  const m = date.getMinutes() + '   ';
   const s = date.getMilliseconds();
-  return Y+ M + D + h + m +s;
+  return Y + M + D + h + m + s;
 }
