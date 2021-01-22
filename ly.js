5cabec0d7418f7447d728fa87ac71a87a8ab81cb
萧宇亨 <xiaoyuheng@haier.com>
2021-01-22 17:54:20 +0800

    更新 ly 网址

--- ly.js
+++ ly.js
@@ -1,3 +1,4 @@
+// https://custom.luyuan.cn/custom/wechat/usercenter.html?code=061x8AFa1WFvnA0vsIGa102mSd3x8AFl&state=STATE&usercode=20210122093525742EVYB54Y
 // 每天签到
 let qdArr = [
   `curl -H 'Host: custom.luyuan.cn' -H 'Accept: */*' -H 'X-Requested-With: XMLHttpRequest' -H 'Accept-Language: zh-cn' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'Origin: http://custom.luyuan.cn' -H 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/7.0.18(0x17001231) NetType/WIFI Language/zh_CN' -H 'Referer: http://custom.luyuan.cn/custom/wechat/integral_index.html?usercode=20210122093525742EVYB54Y&timestamp=${new Date().getTime()}' -H 'Cookie: UM_distinctid=1764bb00e662c3-047fdd2377a0e98-5260777a-4a574-1764bb00e67118' --data-binary "usercode=20210122093525742EVYB54Y" --compressed 'http://custom.luyuan.cn/custom/signin/add'`,
